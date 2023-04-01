2ea90d0b427c90a3333378b873c1ed95fd8c168e
cameron <hammerbro_97@hotmail.com>
2023-03-31 18:28:55 -0600

    tried many things

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let shopping_cart = [];
+
+
 let products = [{
 
 
@@ -59,33 +62,47 @@ let products = [{
 
 function selected(product) {
 
-    let product_array = [];
+let Cookies_get = Cookies.get(`selection`);
 
 
-    
-    Cookies.set(`selection`, product[`target`].getAttribute(`product`));
+
+let addition = string + Cookies_get;
+
+product[`target`].getAttribute(`product`);
+
 
     if (product[`target`].getAttribute(`product`) === `squidward`) {
 
-Cookies.set(`selection`, squid_json);
 
-product_array.push(parse);
+
+
+
+        Cookies.set(`selection`, addition);
 
     } else if (product[`target`].getAttribute(`product`) === `normal`) {
 
-        Cookies.set(`selection`, normal_json);
+        
+        Cookies.set(`selection`, addition);
+        
+       
+        console.log(`added to cart`);
+
+    } else if (product[`target`].getAttribute(`product`) === `horizontal`) {
 
 
-        product_array.push(parse);
+        Cookies.set(`selection`, addition);
+        
+       
+        console.log(`added to cart`);
 
-    } else if(product[`target`].getAttribute(`product`) === `horizontal`){
 
-        Cookies.set(`selection`, horizontal_magikarp);
-        product_array.push(parse);
-    } else if(product[`target`].getAttribute(`product`) === `pixelated`){
+    } else if (product[`target`].getAttribute(`product`) === `pixelated`) {
 
-Cookies.set(`selection`, pixel_json);
-product_array.push(parse);
+        Cookies.set(`selection`, addition);
+        
+       
+        console.log(`added to cart`);
+
     };
 
 };
@@ -97,7 +114,6 @@ for (let counter = 0; counter < product_choice.length; counter = counter + 1) {
 };
 
 
-
 let squidikarp = {
 
 
@@ -105,76 +121,59 @@ let squidikarp = {
     name: `squidikarp`,
 
     price: `$500`,
-    
+
     image_url: `/images/squidward.jpg`,
-    
+
     description: `a horrible byproduct created from the misuse of mutagen`
 
 
-
 };
 
-let normal = {
+let magikarp_normal = {
 
-
+    
     name: `magikarp`,
 
     price: `$100`,
-    
+
     image_url: `/images/normal.png`,
-    
+
     description: `a normal magikarp`
-
-
-
-
 };
 
-
 let horizontal_magikarp = {
-
-
 
     name: `horizontal magikarp`,
 
     price: `$50`,
-    
+
     image_url: `/images/horizontal.jpg`,
-    
+
     description: `a weird magikarp that extists phases in between realities and does not move at all, pretty useless, and probably just dead`
-
-
 
 };
 
-let pixelated_karp = {
-
+let pixelated_magikarp = {
 
 
     name: `pixelated magikarp`,
 
     price: `$325`,
-    
+
     image_url: `/images/pixelated.gif`,
-    
+
     description: `This magikarp doesn't seem to conform to the simulation's set resolution. We don't know how its possible and it's pretty rare`
-    
-    
-    
 
 };
 
 
-
-let squid_json = JSON.stringify(squidikarp);
-
-let normal_json = JSON.stringify(normal);
-
-let horiz_json = JSON.stringify(horizontal_magikarp);
-
-let pixel_json = JSON.stringify(pixelated_karp);
-
-let get_json = Cookies.get(`selection`);
-let parse = JSON.parse(get_json);
+let squid_string = JSON.stringify(squidikarp);
 
 
+let normal_string = JSON.stringify(magikarp_normal);
+
+
+let horzont_string = JSON.stringify(horizontal_magikarp);
+
+
+let pixelated_string = JSON.stringify(pixelated_magikarp);
