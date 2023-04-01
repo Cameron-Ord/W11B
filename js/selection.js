f8855bac562573626a94a5973462aa9362f0116d
cameron <hammerbro_97@hotmail.com>
2023-04-01 00:34:30 -0600

    fixed

--- js/selection.js
+++ js/selection.js
@@ -3,19 +3,18 @@
 
 
 let get_cookie = Cookies.get(`selection`);
-
-let parse_the_json = JSON.parse(get_cookie);
-
 let chosen_products = Cookies.get(`selection`);
 
 //this if statement injects tags based on your cookies//
 
 if (chosen_products === undefined) {
 
-    product_article.insertAdjacentHTML(`afterbegin`, `<h1>Please select a product</h1>`);
+    let product_article = document.querySelector(`#main_section`);
+    product_article.insertAdjacentHTML(`afterbegin`,`<h1>Please select a product</h1>`);
 
 
-} else if (chosen_products !== undefined) {
+
+} else {
 
     //an else if statement looping through all the selected products and displaying them//
 
@@ -23,6 +22,8 @@ if (chosen_products === undefined) {
 
     for (let counter = 0; counter < parse_the_json.length; counter = counter + 1) {
 
+
+        let parse_the_json = JSON.parse(get_cookie);
 
         product_article.insertAdjacentHTML(`beforeend`,
 
